f7954ec1831d80e8995415d6d9cefa595ac70b23
root <root@DESKTOP-JN7MAJ3.localdomain>
2022-12-05 21:14:10 +0530

    added function to check special char in password

--- sample.py
+++ sample.py
@@ -1,10 +1,16 @@
 class account:
-     def check_password_length(self,password):
-         if len(password)>8:
-             return True
-         else:
-             return False
+         def check_password_length(self,password):
+             if len(password)>8:
+                 return True
+             else:
+                 return False
+         def check_spcl_char(self,password):
+            if password.__contains__('*'):
+                return True
+            else:
+                return False
 
 if __name__=='__main__':
     accVerify=account()
     print('Password length is '+str(accVerify.check_password_length('offtoschool')))
+    print('Password conatains special character ' +str(accVerify.check_spcl_char('offtoschool')))
